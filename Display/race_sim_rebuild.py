92df928e25a92a1dfc12f9423555ce785de48767
jameshurst <james.jude.hurst@gmail.com>
2021-07-22 20:02:13 +1000

    added optimised lap counter, as well as various optimisations and bordered cars

--- Display/race_sim_rebuild.py
+++ Display/race_sim_rebuild.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pygame
+import pygame.freetype
 import sector_times
 import team_colours
 import mapPoints
@@ -10,10 +11,10 @@ WINWIDTH = 800  # window width
 WINHEIGHT = 800  # window width
 FRAMERATE = 30
 OFFSET = 50  # map offset from edge
-TRACKNAME = 'Silverstone'
-RACE = 'Silverstone'
+TRACKNAME = 'Monte Carlo'
+RACE = 'Monaco'
 YEAR = 2021
-SPEEDMULT = 10  # speed sim up or down from real time
+SPEEDMULT = 1  # speed sim up or down from real time
 clock = pygame.time.Clock()  # used to keep track of timing and framerate
 screen = pygame.display.set_mode((WINWIDTH, WINHEIGHT), vsync=True)
 
@@ -33,12 +34,12 @@ class Track:
     def draw_map(self, width):
         """simple function that draws the map"""
         for j in range(len(self.track_points)):
-            pygame.draw.line(screen, 'blue', self.track_points[j - 1], self.track_points[j], width)
+            pygame.draw.line(screen, 'red', self.track_points[j - 1], self.track_points[j], width)
 
     def draw_rcing_line(self):
         """ simple function that draws the racing line"""
         for j in range(len(self.race_line_points)):
-            pygame.draw.line(screen, 'red', self.race_line_points[j - 1], self.race_line_points[j], 1)
+            pygame.draw.line(screen, 'blue', self.race_line_points[j - 1], self.race_line_points[j], 1)
 
     def update(self, width=3, rce_line=False):
         """method to be called once per frame to draw track."""
@@ -54,10 +55,15 @@ class Car:
         self.name = car_name
         self.data = car_data  # position and sector time data
         self.coords, self.time_arr = self.data.race_points_times(self.name)  # TODO: Add cache to speed up
+        self.lap_data = self.data.track_laps.pick_driver(self.name)
+        self.ref_time = self.lap_data.iloc[0].LapStartTime
+        self.updates = int(car_name)
 
         self.x = self.coords[0][0]  # x position on map
         self.y = self.coords[0][1]  # y position on map
         self.indices_checked = 0  # variable used to ignore indices that have already been checked
+        self.laps_checked = 0
+        self.laps = 1
 
         self.track = track
 
@@ -74,9 +80,13 @@ class Car:
 
     def update(self, runtime):
         """method that takes the given race time and calculates the position that the car is"""
-        lower_index = self._return_index(runtime, self.time_arr)  # indices for the position data array
+        lower_index = self._get_index(runtime, self.time_arr)  # indices for the position data array
         upper_index = self._find_upper_index(lower_index)
         fraction = self._get_fraction(upper_index, lower_index, runtime)  # fraction between one point and the next
+        if (self.updates % 10) == 0:
+            self.laps = int(self._get_laps(runtime))
+            self.updates -= 10
+        self.updates += 1
 
         # calculates difference between the two closest points
         delta_x = self.coords[upper_index][0] - self.coords[lower_index][0]
@@ -90,6 +100,7 @@ class Car:
 
     def _draw(self):
         """simple method that draws the car"""
+        pygame.draw.circle(screen, [0, 0, 0], (self.x, self.y), 7)
         pygame.draw.circle(screen, self.car_colour, (self.x, self.y), 5)
 
     def _get_fraction(self, upper_index, lower_index, runtime):
@@ -117,7 +128,7 @@ class Car:
             upper_index = lower_index + 1
         return upper_index
 
-    def _return_index(self, runtime, time_array):
+    def _get_index(self, runtime, time_array):
         """finds the lower index of the telemetry that corresponds to the given runtime."""
         timedelta = pd.Timedelta(runtime, 's')
         compare_time = timedelta + self.data.ref_time  # time since start of the race
@@ -132,6 +143,24 @@ class Car:
                 return self.indices_checked
         return self.indices_checked
 
+    def _get_laps(self, runtime):
+        max_indices = len(self.lap_data)
+        timedelta = pd.Timedelta(runtime, 's')
+        compare_time = timedelta + self.ref_time  # time since start of the race
+        added_index = 0
+        try:
+            while self.lap_data.iloc[added_index + self.laps_checked].LapStartTime < compare_time:
+                this_lap_start_time = self.lap_data.iloc[added_index + 1].LapStartTime
+                if added_index < (max_indices - 1) and this_lap_start_time < compare_time:
+                    added_index += 1
+                else:
+                    self.laps_checked += added_index
+                    return self.laps_checked
+        except IndexError:
+            pass
+        return self.lap_data.iloc[added_index].LapNumber
+
+
 
 class Race:
     """Class that controls the race"""
@@ -139,7 +168,7 @@ class Race:
         pygame.init()
         self.track_obj = Track(TRACKNAME)
         self.race_lap_data = sector_times.LapData(YEAR, RACE, 'R')
-        self.drivers = self.race_lap_data.drivers_list()  # list of names of drivers # ['44']
+        self.drivers = self.race_lap_data.drivers_list()  # list of names of drivers ['44'] #
         self.cars = []
 
         self.finished = False
@@ -149,6 +178,9 @@ class Race:
         self.Speedmult = SPEEDMULT
         self.time_running = 0
 
+        self.REG_FONT = pygame.freetype.Font("F1 Font Files (with important Message)/Formula1-Regular.otf", 24)
+        self.WIDE_FONT = pygame.freetype.Font("F1 Font Files (with important Message)/Formula1-Wide.otf", 24)
+
     def _init_drivers_if_empty(self):
         """initilaises the cars and writes them into a list"""
         if len(self.cars) > 0:
@@ -156,6 +188,19 @@ class Race:
         for driver_names in self.drivers:
             self.cars.append(Car(driver_names, self.track_obj, self.race_lap_data))
             pygame.display.flip()
+
+    def draw_laps(self):
+        lap_number = 1
+        try:
+            for cars in self.cars:
+                lap_number = int(max(cars.laps, lap_number))
+        except IndexError:
+            pass
+        lap = str(lap_number)
+        total_laps = ' / 52'  # TODO: find method to find the no. laps
+        bottom_line = lap + total_laps
+        self.REG_FONT.render_to(screen, (40, 100), bottom_line, (255, 255, 255))
+        self.WIDE_FONT.render_to(screen, (31, 50), "LAP", (255, 255, 255))
 
     def _check_if_quitting(self):
         """checks if have received a request to quit the window"""
@@ -176,7 +221,7 @@ class Race:
             self.track_width -= 1
             print(self.track_width)
         elif pressed[pygame.K_d]:
-            if self.Speedmult < 20:
+            if self.Speedmult < 4500:
                 self.Speedmult += 1
             print(self.Speedmult)
         elif pressed[pygame.K_a]:
@@ -186,12 +231,17 @@ class Race:
         elif pressed[pygame.K_j]:
             self.draw_racing_line = not self.draw_racing_line
 
+    def _draw_screen(self):
+        screen.fill((150, 150, 150))
+
+
     def _wait_and_draw(self):
         """draws the map and the cars when waiting for the race to start"""
-        screen.fill((0, 0, 0))
+        self._draw_screen()
         self._check_if_quitting()
         self._check_key_presses()
         self.track_obj.update(self.track_width)
+        self.draw_laps()
         if self.draw_racing_line:
             self.track_obj.draw_rcing_line()
         self._init_drivers_if_empty()
@@ -202,10 +252,11 @@ class Race:
 
     def _race_and_draw(self):
         """does the race loop"""
-        screen.fill((0, 0, 0))
+        self._draw_screen()
         self._check_key_presses()
         self.track_obj.update(self.track_width)
         self._check_if_quitting()
+        self.draw_laps()
         for i in self.cars:
             i.update(self.time_running)
         pygame.display.flip()
@@ -226,3 +277,5 @@ class Race:
 if __name__ == "__main__":
     Main_race = Race()
     Main_race.race()
+    # Test_car = Car('44', Track('Silverstone'), sector_times.LapData(2021, 'Silvestone', 'R'))
+    # print(Test_car._get_laps(300))
